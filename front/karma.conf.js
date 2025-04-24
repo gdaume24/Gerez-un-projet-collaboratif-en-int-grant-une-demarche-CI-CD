f1cc82b2bc4932f62e8205f911cfaeac55e56ab5
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-04-24 07:57:15 +0200

    no sandbox no wwatch no progress

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -35,11 +35,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome", "ChromeHeadless", "ChromeHeadlessCI"],
+    browsers: ["Chrome_without_sandbox"],
     customLaunchers: {
-      ChromeHeadless: {
+      Chrome_without_sandbox: {
         base: "ChromeHeadless",
-        flags: ["--no-sandbox"],
+        flags: ["--no-sandbox"], // with sandbox it fails under Docker
       },
     },
     singleRun: true,
