19776838c6c47353675d6583cdabd5f0519d0adf
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-04-24 10:28:47 +0200

    m

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -34,6 +34,7 @@ module.exports = tseslint.config(
   },
   {
     files: ["**/*.html"],
+
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
